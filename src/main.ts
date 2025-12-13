4f69dc4961cfd1440477042b63b6e57aa03f8b5f
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-13 15:02:55 +0700

    feat: update server port and enhance logging format in main application

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
+import 'reflect-metadata'
 import 'dotenv/config'
 import express from 'express'
 import { DatabaseModule } from './database/database.module'
 import { RouteModule } from './modules/route.module'
+import morgan from 'morgan'
 
 const bootstrap = async () => {
 	const app = express()
 
 	const HOST: string = process.env.HOST || '127.0.0.1'
-	const PORT: string | number = process.env.PORT || 2004
+	const PORT: string | number = process.env.PORT || 3198
 
 	app.use(express.json())
 	app.use(express.urlencoded())
+	app.use(morgan('dev'))
 
 	await Promise.all([DatabaseModule.register(), RouteModule.forRootAsync({ prefix: '/v1/api', app })])
 
 	await app.listen(PORT, () => {
-		console.log(`Server is running on http://${HOST}:${PORT}`)
+		console.log(`[Application] :>>> Server is running on http://${HOST}:${PORT}`)
 	})
 }
 
