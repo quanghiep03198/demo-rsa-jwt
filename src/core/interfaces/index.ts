4ae64eeb4e08f974b9e46437737d9ffd34eac951
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-13 15:03:26 +0700

    feat: add IApplicationMiddleware interface for middleware handling in the application

--- src/core/interfaces/index.ts
+++ src/core/interfaces/index.ts
@@ -1,5 +1,9 @@
-import { Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 
 export interface IBaseController {
 	router: Router
+}
+
+export interface IApplicationMiddleware {
+	handle(req: Request, res: Response, next: NextFunction): any
 }
